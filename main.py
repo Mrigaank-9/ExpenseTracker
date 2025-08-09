96157b1d96cc40b14d3fcd51537c1dc738d93180
Mrigaank-9 <130607343+Mrigaank-9@users.noreply.github.com>
2025-08-09 23:20:26 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import pandas as pd
+
+from backend.Expenses.expenses import router as router_expense
+
+app = FastAPI()
+
+'''
+{
+  "expense_date": "2025-08-09",
+  "category": "Food",
+  "amount": 250.75,
+  "description": "Dinner at Italian restaurant",
+  "payment_method": "Credit Card",
+  "currency": "INR",
+  "created_at": "2025-08-09"
+}
+
+'''
+
+app.include_router(router_expense)
